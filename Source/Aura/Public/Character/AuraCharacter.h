bb22a41a9b142f5f4cc5dd6df3be7035dc550451
oprincipe <orazio.principe@gmail.com>
2025-10-19 12:06:17 +0100

    Refactor and extend character hierarchy: split `AuraCharacterBase`, add `AuraCharacter` and `AuraEnemy` classes

---
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -0,0 +1,24 @@
+﻿// Copyright NattySoft
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+UCLASS()
+class AURA_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	AAuraCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
---
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,24 @@
+﻿// Copyright NattySoft
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "AuraEnemy.generated.h"
+
+UCLASS()
+class AURA_API AAuraEnemy : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	AAuraEnemy();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
