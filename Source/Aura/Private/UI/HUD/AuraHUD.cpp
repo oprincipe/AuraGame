0cd40ae27a2b4ac3ecb0d8587e1331ea71376688
oprincipe <orazio.principe@gmail.com>
2025-10-20 15:41:50 +0100

    Add `AAuraHUD` class for overlay widget management
    Create `AAuraHUD` class with functionality to initialize and display an overlay widget via `UAuraUserWidget`. Add associated blueprint `BP_AuraHUD` and update `BP_AuraGameMode` to utilize the new HUD system.

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,23 @@
+﻿// Copyright NattySoft
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "Blueprint/UserWidget.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Null checks for safety
+	if (GetWorld() && OverlayWidgetClass)
+	{
+		// Create widget with correct type and store reference
+		OverlayWidget = CreateWidget<UAuraUserWidget>(GetWorld(), OverlayWidgetClass);
+		if (OverlayWidget)
+		{
+			OverlayWidget->AddToViewport();
+		}
+	}
+}
