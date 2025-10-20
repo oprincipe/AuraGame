92f07a0dfc75af21cdb1851242fbbc51d1308065
oprincipe <orazio.principe@gmail.com>
2025-10-20 15:32:05 +0100

    Add `UAuraWidgetController` and `UAuraUserWidget` classes for UI management
    Introduce `UAuraWidgetController` for managing player- and ability-related widget data. Implement `UAuraUserWidget` with support for setting widget controllers. Update build file to include `UMG` module. Add initial UI blueprints for health, mana, and overlay widgets.

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright NattySoft
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
