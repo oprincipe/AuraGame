4eec31d8a8d9451b4f72b5cfa7b4b627be77f975
oprincipe <orazio.principe@gmail.com>
2025-11-05 08:03:04 +0000

    Add `UAuraProjectileSpell` class and initial implementation
    Introduce `UAuraProjectileSpell` derived from `UAuraGameplayAbility` with an overridden `ActivateAbility` method. Add debug logging for ability activation. Update `BP_AuraCharacter` and `StartupMap` for integration. Add `GA_FireBolt` asset for related gameplay functionality.

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright NattySoft
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                           const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                           const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UKismetSystemLibrary::PrintString(this, FString("Activate ability c++"), true, true, FColor::Green, 3.f);
+}
