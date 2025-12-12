2e8740c6a175615dc3db2c0aafbd2c929e00fe6d
oprincipe <orazio.principe@gmail.com>
2025-12-12 16:53:45 +0000

    Add Ghoul enemy assets and update combat properties
    - Added new Ghoul enemy blueprint (`BP_Ghoul`) and associated animations (`AM_Ghoul_Attack_L_Montage`, `AM_Ghoul_Attack_R_Montage`, `AM_HitReact_Ghoul_Montage`).
    - Introduced animation blueprint (`ABP_Ghoul`) and blend space (`BS_Idle_Walk`) for Ghoul movement.
    - Adjusted `BaseWalkSpeed` to `EditAnywhere` and removed `EditAnywhere` from `CombatTarget` in `AuraEnemy`.

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -50,13 +50,13 @@ public:
 	UPROPERTY(BlueprintReadOnly, Category="Combat")
 	bool bHitReacting = false;
 	
-	UPROPERTY(BlueprintReadOnly, Category="Combat")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Combat")
 	float BaseWalkSpeed = 250.f;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Combat")
 	float LifeSpan = 5.f;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Combat")
+	UPROPERTY(BlueprintReadWrite, Category="Combat")
 	TObjectPtr<AActor> CombatTarget;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Combat")
