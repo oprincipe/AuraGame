d4103010bf1452be02b20bc6c05ed6fba9a5c54b
oprincipe <orazio.principe@gmail.com>
2025-12-17 16:09:44 +0000

    Add `AuraSummonAbility` for spawn location calculation and update related assets
    - Introduced `UAuraSummonAbility` class to

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
@@ -0,0 +1,36 @@
+﻿// Copyright NattySoft
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraGameplayAbility.h"
+#include "AuraSummonAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraSummonAbility : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetSpawnLocations();
+
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	int32 NumMinions = 5;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	TArray<TSubclassOf<APawn>> MinionClasses;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	float MinSpawnDistance = 50.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	float MaxSpawnDistance = 250.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Summoning")
+	float SpawnSpreadAngle = 90.f;
+	
+};
