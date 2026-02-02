dd98f3995bf9e788097339807ea4ef2e131918f9
oprincipe <orazio.principe@gmail.com>
2026-02-02 17:21:10 +0000

    Add `AAuraMagicCircle` actor and integrate magic circle functionality
    - Introduced `AAuraMagicCircle` actor with a `UDecalComponent` to represent the visual magic circle.
    - Added `BP_MagicCircle` asset for Arcane abilities.
    - Updated `Doungeon` map and `NS_ArcaneShards` asset to incorporate the magic circle visuals.

---
+++ Source/Aura/Private/Actor/AuraMagicCircle.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright NattySoft
+
+
+#include "Actor/AuraMagicCircle.h"
+
+#include "Components/DecalComponent.h"
+
+AAuraMagicCircle::AAuraMagicCircle()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	MagicCircleDecal = CreateDefaultSubobject<UDecalComponent>("MagicCircleDecal");
+	MagicCircleDecal->SetupAttachment(GetRootComponent());
+}
+
+void AAuraMagicCircle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AAuraMagicCircle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
